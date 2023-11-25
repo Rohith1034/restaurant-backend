a8bbb6a3b028beec865c733d4455c77098782c28
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2023-11-25 10:58:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userSchema = require("./schema/user");
+const bcrypt = require("bcrypt");
+const md5 = require("md5");
+
+
+
+const saltRounds = 10;
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Create a mongoose model from the userSchema
+const User = mongoose.model("User", userSchema);
+
+app.post("/data", async (req, res) => {
+    try {
+        const data = req.body;
+        
+        const newUser = new User({
+            name: data.name,
+            email: data.email,
+            phone: data.phone,
+            password: md5(data.confirmPassword),
+            cart: "",
+        });
+        await newUser.save();
+    } catch (error) {
+        console.error("Error handling data:", error);
+        res.status(500).json({ error: "An error occurred while processing the data" });
+    }
+});
+
+app.post("/userdata", async (req, res) => {
+    try {
+        const usesCollection = mongoose.connection.collection("users");
+        const userData = await usesCollection.find({}).toArray();
+        res.json(userData);
+    }
+
+    catch(error) {
+        console.log(error);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+const main = async () => {
+    try {
+        await mongoose.connect(
+            process.env.MONGO_URL,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
+        console.log("Connected to MongoDB successfully");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+main();
