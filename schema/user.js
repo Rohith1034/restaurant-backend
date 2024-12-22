0f15e48c33cb94d2fb9fd777dd41b167c9a709e1
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2024-12-22 22:48:24 +0530

    Update user.js

--- schema/user.js
+++ schema/user.js
@@ -5,29 +5,43 @@ const userSchema = new mongoose.Schema({
   email: String,
   phone: String,
   password: String,
-  cart: [String],
+  cart: [
+    {
+      itemId: String,  // Unique identifier for the item
+      name: String,    // Name of the item
+      price: Number,
+      image: String,   // Price of the item
+      quantity: {      // Quantity of the item in the cart
+        type: Number,   // Default quantity is 1 if not specified
+      }
+      
+    }
+  ],
+  profile_pic: String,
   recently_viewed: [String],
-  "address": {
-    "street": String,
-    "city": String,
-    "state": String,
-    "zipCode": String,
-    "country": String
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String
   },
   admin: Boolean,
-  orders: [{
-    "orderId": String,
-    "restaurantId": String,
-    "items": [
-      {
-        "itemId": String,
-        "name": String,
-        "quantity": Number,
-        "price": Number,
-      }
-    ],
-    "totalAmount": Number,
-  }],
+  orders: [
+    {
+      orderId: String,
+      items: [
+        {
+          itemId: String,
+          name: String,
+          quantity: Number,
+          price: Number,
+          img: String
+        }
+      ],
+      totalAmount: Number,
+    }
+  ],
 });
 
 module.exports = userSchema;
